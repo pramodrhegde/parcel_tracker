fa684a3f4ad5d5001aaf03155b26b34bc403f2a7
pramodrhegde <pramod.rh@gmail.com>
2015-11-27 21:19:08 +0530

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,4 @@
-var parcelItemList = [];
-
 $(document).ready(function(){
-
 	//parcel details
 	$.ajax({
 		type:"GET",
@@ -9,8 +6,39 @@ $(document).ready(function(){
 		cache:false,
 		dataType:"json",
 		success:function(data){
-			var parcelItemMarkup = '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">';
-			parcelItemList = data.parcels;
+			console.log('success');
+			var parcelTemplateScript = $('#parcel-template').html();
+			var parcelTemplate = Handlebars.compile(parcelTemplateScript);
+			var parcelHtml = parcelTemplate(data);
+			$('.main > .row').html(parcelHtml).find('div').last().addClass('end');
+
+			$('.parcel-item a').on('click',function(){
+
+				var modalTemplateScript = $('#modal-template').html();
+				var modalTemplate = Handlebars.compile(modalTemplateScript);
+				
+				var $this = $(this),
+				    data = $this.parent('.parcel-item'),
+					templateData = {
+					    name : data.find('p[data-name]').data('name'),
+					    date : data.find('p[data-date]').data('date'),
+					    type : data.find('p[data-type]').data('type'),
+					    weight : data.find('p[data-weight]').data('weight'),
+					    price : data.find('p[data-price]').data('price'),
+					    quantity : data.find('p[data-qty]').data('qty'),
+					    image : data.find('p[data-image]').data('image'),
+					    color : data.find('p[data-color]').data('color'),
+					    phone : data.find('p[data-phone]').data('phone')
+					};
+
+				var modalHtml = modalTemplate(templateData);
+				$('.product-info').remove();
+				$('#exampleModal1').prepend(modalHtml);
+				//$('#exampleModal1').foundation('reveal','open');
+				$('#map_canvas').empty();
+				getCoordinates($this.parent('.parcel-item').data('uid'));
+				
+			});
 		},
 		error:function(xhr,status,error){
 			alert('error');
@@ -31,3 +59,74 @@ $(document).ready(function(){
 		}
 	});
 });
+
+function getCoordinates(uid){
+	$.ajax({
+		type:"GET",
+		url:"http://zoomcar-ui.0x10.info/api/courier?type=json&query=list_parcel",
+		cache:false,
+		dataType:"json",
+		success:function(data){
+			for(var i=0;i<data.parcels.length;i++){
+				if(data.parcels[i].color===uid){
+					createMap(data.parcels[i].live_location.latitude,data.parcels[i].live_location.longitude);
+					return;
+				}
+			}
+		},
+		error:function(xhr,status,error){
+		}
+	});
+}
+
+function createMap(lat,lng){
+	latlng = new google.maps.LatLng(lat, lng),
+    image = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png';
+    
+    var mapOptions = {
+             center: new google.maps.LatLng(lat, lng),
+             zoom: 13,
+             mapTypeId: google.maps.MapTypeId.ROADMAP,
+             panControl: true,
+             panControlOptions: {
+                 position: google.maps.ControlPosition.TOP_RIGHT
+             },
+             zoomControl: true,
+             zoomControlOptions: {
+                 style: google.maps.ZoomControlStyle.LARGE,
+                 position: google.maps.ControlPosition.TOP_left
+             }
+         },
+        map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions),
+        marker = new google.maps.Marker({
+                 position: latlng,
+                 map: map,
+                 icon: image
+             });
+
+    google.maps.event.addListener(map, 'click', function (event) {
+         infowindow.close();
+         var geocoder = new google.maps.Geocoder();
+         geocoder.geocode({
+             "latLng":event.latLng
+         }, function (results, status) {
+             console.log(results, status);
+             if (status == google.maps.GeocoderStatus.OK) {
+                 console.log(results);
+                 var lat = results[0].geometry.location.lat(),
+                     lng = results[0].geometry.location.lng(),
+                     placeName = results[0].address_components[0].long_name,
+                     latlng = new google.maps.LatLng(lat, lng);
+
+                 moveMarker(placeName, latlng);
+             }
+         });
+    });
+}
+
+function moveMarker(placeName, latlng) {
+     marker.setIcon(image);
+     marker.setPosition(latlng);
+     infowindow.setContent(placeName);
+     //infowindow.open(map, marker);
+ }
